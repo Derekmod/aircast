53e0b1389254ba7c3fe0aa6f5a7c2945a71e8f9b
nkumarcc <nkumarcc@gmail.com>
2023-09-09 13:55:09 -0700

    Abstract content

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { InfiniteScrollModule } from "ngx-infinite-scroll";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OverviewComponent } from './overview/overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
