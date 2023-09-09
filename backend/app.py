8025785ad1e2409139af1daa9769e5d626b9c176
Derekmod <derekmod@gmail.com>
2023-09-09 09:57:24 -0700

    Initial backend (#2)

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<html><head><title>Hello World</title></head><body><h1>Hello, World!</h1></body></html>"
+
+
+# Call with `python backend/app.py`
+if __name__ == "__main__":
+    app.run()
