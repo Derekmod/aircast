8999694a353eca40977a1b5a7843fb9b74025abe
Derek Modzelewski <derekmod@gmail.com>
2023-09-09 13:09:29 -0700

    .

--- backend/curriculum_generation.py
+++ backend/curriculum_generation.py
@@ -1,13 +1,45 @@
 from backend.curriculum import PaperCurriculum, Question, Module
 from backend.paper_parsing import ParsedPaper
+from claude.generator import generate, Trajectory, Turn
+import yaml
+import any_serde
+from utils.jinja_utils import load_template, get_render_args
+
+from utils.path_utils import PROJECT_ROOT_PATH
+
+
+MESSAGES_DIR = PROJECT_ROOT_PATH / "backend" / "messages"
+
+
+def get_blurb(parsed_content: ParsedPaper) -> str:
+    with (MESSAGES_DIR / "initial_trajectory.yaml").open("rt") as fin_traj:
+        traj_data = yaml.load(fin_traj, Loader=yaml.SafeLoader)
+
+    traj = any_serde.from_data(Trajectory, traj_data)
+
+    prompt_template = load_template(MESSAGES_DIR / "blurb_prompt.jinja2")
+    prompt = prompt_template.render(**get_render_args(locals()))
+
+    traj.turns.append(
+        Turn(
+            user_message=prompt,
+            assistant_message=None,
+        )
+    )
+
+    response = generate(traj)
+
+    return response
 
 
 def generate_curriculum(parsed_content: ParsedPaper) -> PaperCurriculum:
-    del parsed_content
+    blurb = get_blurb(parsed_content)
+
     return PaperCurriculum(
         overview_module=Module(
             name="overview",
-            blurb="The paper is about things and things are cool.",
+            # blurb="The paper is about things and things are cool.",
+            blurb=blurb,
             questions=[
                 Question(
                     question="Why is the paper cool?",
